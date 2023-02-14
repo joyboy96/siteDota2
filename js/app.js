c2ae1882179a64dea92b00b7ec8c7c8c58e92d02
Daniyar <53312288+joyboy96@users.noreply.github.com>
2023-02-14 13:23:17 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+// slider1 = new Swiper ('.slider1', {
+//     freeMode: true,
+//     centeredSlides: true,
+//     direction: 'vertical',
+//     mousewheel: true,
+//     slidesPerView: 1.75,
+// })
 
+
+document.querySelectorAll('.slider').forEach((n, i) => {
+	window[`slider${i+1}`] = new Swiper(n, {
+		freeMode: true,
+		centeredSlides: true,
+		direction: 'vertical',
+		mousewheel: true,
+		slidesPerView: 1.75,
+		slidesOffsetBefore: -150
+	})
+})
+bindSwipers(slider1, slider2, slider3, slider4)
